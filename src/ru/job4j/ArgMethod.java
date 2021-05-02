807d7b94d69b7ad64caf8b44b02bbfbe864414db
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-05-02 12:44:29 +0300

    4.1. Аргументы [#295521]

---
+++ src/ru/job4j/ArgMethod.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void hello (String name, int age) {
+        System.out.println("Hello, " + name + "," + age + " years old.");
+    }
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+        ArgMethod.hello(name, age);
+    }
+}
