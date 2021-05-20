deec482f55768c7d39ffce2e2f3d0b170176b61e
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-05-20 22:03:59 +0300

    5.6. Простые числа [#156314]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,12 +3,9 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = number > 1;
-        for (int i = 2; i <= number; i++) {
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
-                break;
-            } else {
-                prime = true;
                 break;
             }
         }
--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -27,4 +27,11 @@ public class CheckPrimeNumberTest {
         boolean result = CheckPrimeNumber.check(number);
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void when9() {
+        int number = 9;
+        boolean result = CheckPrimeNumber.check(number);
+        Assert.assertFalse(result);
+    }
 }
