846c87ede8891c63c2c5b6f81db71fc52b79047a
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-05-24 18:55:50 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Yuriy Rozonov";
+        names[2] = "Vasiliy Solovev";
+        names[3] = "Fatkhullo Sharofidinov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
